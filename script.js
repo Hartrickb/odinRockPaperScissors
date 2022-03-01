0187932c6ad0eb5344873f42576e3bf97546a52d
Hartrickb <hartrickb@users.noreply.github.com>
2022-03-01 15:39:30 -0500

    playerPlay and playRound

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Randomly Selects Computers Play
 function computerPlay() {
   const rockPaperScissors = {
     0: 'Rock',
@@ -5,4 +6,29 @@ function computerPlay() {
     2: 'Scissors',
   };
   return rockPaperScissors[Math.floor(Math.random() * 3)];
+}
+
+// Prompts the user to make a pick
+function playerPlay() {
+  let selection = prompt('Rock, Paper, or Scissors?');
+  selection = selection[0].toUpperCase() + selection.slice(1).toLowerCase();
+  return selection;
+}
+
+// Calculates who wins a round
+// Rock > Scissors
+// Paper > Rock
+// Scissors > Paper
+function playRound(playerSelection, computerSelection) {
+  if (
+    (playerSelection === 'Rock' && computerSelection === 'Scissors') ||
+    (playerSelection === 'Paper' && computerSelection === 'Rock') ||
+    (playerSelection === 'Scissors' && computerSelection === 'Paper')
+  ) {
+    console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+    return 'player';
+  } else {
+    console.log(`You lose. ${computerSelection} beats ${playerSelection}`);
+    return 'computer';
+  }
 }
