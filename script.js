ccbea3bee86b4b304e202c74d466f6931c734846
Hartrickb <hartrickb@users.noreply.github.com>
2022-03-02 20:28:11 -0500

    buttons and outcome

--- script.js
+++ script.js
@@ -20,27 +20,46 @@ function playerPlay() {
 // Paper > Rock
 // Scissors > Paper
 function playRound(playerSelection, computerSelection) {
+  const outcomeDiv = document.querySelector('#outcome');
   if (
     (playerSelection === 'Rock' && computerSelection === 'Scissors') ||
     (playerSelection === 'Paper' && computerSelection === 'Rock') ||
     (playerSelection === 'Scissors' && computerSelection === 'Paper')
   ) {
     console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+    outcomeDiv.style.backgroundColor = 'green';
+    outcomeDiv.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
     return 'player';
+  } else if (playerSelection === computerSelection) {
+    console.log('You tied. Try again.');
+    outcomeDiv.style.backgroundColor = 'gray';
+    outcomeDiv.textContent = 'You tied. Try again.';
+    return 'tie';
   } else {
     console.log(`You lose. ${computerSelection} beats ${playerSelection}`);
+    outcomeDiv.style.backgroundColor = 'red';
+    outcomeDiv.textContent = `You lose. ${computerSelection} beats ${playerSelection}`;
     return 'computer';
   }
 }
 
 // Plays five rounds and prints an overall winner
 function game() {
-  let gameWinner = { player: 0, computer: 0 };
-  for (let i = 0; i < 5; i++) {
-    const computerSelection = computerPlay();
-    const playerSelection = playerPlay();
-    const winner = playRound(playerSelection, computerSelection);
-    gameWinner[winner] = gameWinner[winner] + 1;
-  }
-  console.log(gameWinner);
+  // let gameWinner = { player: 0, computer: 0 };
+  // for (let i = 0; i < 5; i++) {
+  //   const computerSelection = computerPlay();
+  //   const playerSelection = playerPlay();
+  //   const winner = playRound(playerSelection, computerSelection);
+  //   gameWinner[winner] = gameWinner[winner] + 1;
+  // }
+  // console.log(gameWinner);
 }
+
+const buttons = document.querySelectorAll('.playGame');
+console.log(buttons);
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    console.log(button.id);
+    playRound(button.id, computerPlay());
+  });
+});
